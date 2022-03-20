44c58fa2cddf6eb396700ba4e09409632b4e8c7f
bmdeaver <brentmdeaver@gmail.com>
2022-03-20 15:59:40 -0700

    Remove console output and add comments

--- advice-generator-app-main/deaver-advice-generator/script.js
+++ advice-generator-app-main/deaver-advice-generator/script.js
@@ -2,20 +2,21 @@ const apiUrl = 'https://api.adviceslip.com/advice';
 const idElement = document.getElementById('id');
 const quoteElement = document.getElementById('quote');
 
+/* Use api to get a random quote */
 async function getRandomQuote() {
     const response = await fetch('https://api.adviceslip.com/advice');
-
     var data = await response.json();
 
-    console.log(data.slip.id);
-    console.log(data.slip.advice)
-    console.log(data.slip);
-    idElement.innerHTML = data.slip.id;
-    quoteElement.innerHTML = `\"${data.slip.advice}\"`;
+    if(data) {
+        idElement.innerHTML = data.slip.id;
+        quoteElement.innerHTML = `\"${data.slip.advice}\"`;
+    }
 }
 
+/* Get random quote on app load */
 getRandomQuote();
 
+/* Listen for click events and update the quote */
 document.getElementById('dice-container').addEventListener('click', () => {
     getRandomQuote();
 })
